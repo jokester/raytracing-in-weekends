49f6225d6949a761c46067b76e87ed25dc0c88b6
Wang Guan <momocraft@gmail.com>
2020-07-12 01:34:41 +0900

    True Lambertian Reflection

--- src/main/scala/io/jokester/raytracing1weekend/Main.scala
+++ src/main/scala/io/jokester/raytracing1weekend/Main.scala
@@ -17,7 +17,7 @@ object Main extends App with LazyLogging {
       imgW,
       imgH
     ),
-    3,
+    10,
     Seq(
       Sphere(Vec3(0, -0.1, -1), 0.5),
       Sphere(Vec3(0, -100.5, -1), 100)
--- src/main/scala/io/jokester/raytracing1weekend/Threaded.scala
+++ src/main/scala/io/jokester/raytracing1weekend/Threaded.scala
@@ -7,13 +7,20 @@ object Threaded {
   def random: Random                       = _random.get()
 
   @scala.annotation.tailrec
-  def randomUnit(): Vec3 = {
+  def randomUnitSphere(): Vec3 = {
     val r = Vec3(
       randomDouble(-1, 1),
       randomDouble(-1, 1),
       randomDouble(-1, 1)
     )
-    if (r.length < 1) r else randomUnit()
+    if (r.length < 1) r else randomUnitSphere()
+  }
+
+  def randomUnit(): Vec3 = {
+    val a = randomDouble(0, Math.PI * 2)
+    val z = randomDouble(-1, 1)
+    val r = Math.sqrt(1 - z*z)
+    Vec3(r * Math.cos(a), r * Math.sin(a), z)
   }
 
   @inline
