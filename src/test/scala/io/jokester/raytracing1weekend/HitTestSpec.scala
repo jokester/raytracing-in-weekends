88db4fa11d2e3ac7fa09e4de6bf46ce2e6639d2b
Wang Guan <momocraft@gmail.com>
2020-07-24 22:49:59 +0900

    fix tests

--- src/test/scala/io/jokester/raytracing1weekend/HitTestSpec.scala
+++ src/test/scala/io/jokester/raytracing1weekend/HitTestSpec.scala
@@ -7,17 +7,18 @@ class HitTestSpec extends AnyFlatSpec with Matchers {
 
   "Ray and Sphere" should "find intersection point" in {
     val ray = Ray(Vec3(0, 0, 0), Vec3(0, 1, 0))
+    val material = Metal(Color3.black, 0)
 
     // not hit
-    Sphere(Vec3(0, 0, 5), 1).hitBy(ray, -1, 1e6) shouldEqual None
+    Sphere(Vec3(0, 0, 5), 1, material).hitBy(ray, -1, 1e6) shouldEqual None
 
     // 2 intersections
-    val sphere = Sphere(Vec3(0, 1, 0), 1)
+    val sphere = Sphere(Vec3(0, 1, 0), 1, material)
     sphere.hitBy(ray, -1, 1e6) shouldEqual Some(
-      HitRecord(Vec3(0, 0, 0), Vec3(0, -1, 0), 0))
+      HitRecord(Vec3(0, 0, 0), Vec3(0, -1, 0), 0, material))
 
     sphere.hitBy(ray, 0.01, 1e6) shouldEqual Some(
-      HitRecord(Vec3(0, 2, 0), Vec3(0, 1, 0), 2))
+      HitRecord(Vec3(0, 2, 0), Vec3(0, 1, 0), 2, material))
 
     sphere.hitBy(ray, 0.01, 1.99) shouldEqual None
   }
